4523f578fee6a4ac97c3ce6fb43b6cacc51c0731
unknown <bvives@iesemilidarder.com>
2024-12-02 13:12:30 +0100

    Initial

---
+++ BLOC2/blog3/database/factories/PostFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->sentence();
+        return [
+            'title' => $title,
+            'url_clean' => Str::slug($title),
+            'content' => fake()->randomHtml(2,3),
+            'posted' => fake()->boolean ? 'yes' : 'not',
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'user_id' => User::inRandomOrder()->first()->id,
+        ];
+    }
+}
