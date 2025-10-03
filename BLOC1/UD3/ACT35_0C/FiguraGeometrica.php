9da7d29b9be920e767951b819b7c689de9172861
unknown <bvives@iesemilidarder.com>
2025-10-03 11:08:38 +0200

    Initial

--- BLOC1/UD3/ACT35_0C/FiguraGeometrica.php
+++
@@ -1,7 +0,0 @@
-<?php trait FiguraGeometrica {
-    abstract public function calculaArea(): float;  // declarar, no implementar
-
-    public function nomFigura(): string {  // mètode propi del trait 'FiguraGeometrica'
-        return get_class($this);
-    }
-} ?>
