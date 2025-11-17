be41e4e8e697c93c172dc829cdec048a7b3a59cd
unknown <bvives@iesemilidarder.com>
2025-11-17 14:09:26 +0100

    Initial

--- BLOC2/blog/database/seeders/PostSeeder.php
+++ BLOC2/blog/database/seeders/PostSeeder.php
@@ -34,14 +34,14 @@ class PostSeeder extends Seeder
 
         // Crear 'Tags'
         foreach ($tags as $tag) {
-            Tag::create(['name' => $tag]);  // firstOrCreate() per evitar duplicats
+            Tag::create(['name' => $tag]);  // 'firstOrCreate()' per evitar duplicats
         }
 
         // Crear 'Posts' 
         foreach ($posts as $post) {
             $post = Post::create([
                 'title'       => $post,
-                'url_clean'   => $post, // EXISTEIX UN MUTADOR: strtolower(str_replace(' ', '_', $post)),
+                'url_clean'   => $post, // EXISTEIX UN MUTADOR: 'strtolower(str_replace(' ', '_', $post)),'
                 'content'     => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
                 'user_id'     => User::inRandomOrder()->first()->id,  // assignar un usuari aleatori
                 'category_id' => Category::inRandomOrder()->first()->id,  // assignar una categoria aleatòria
