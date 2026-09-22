ed382fa5948715690feaff4fe56254e760ffff1d
bvivess <bvives@iesemilidarder.com>
2026-09-22 12:05:28 +0200

    Initial

--- BLOC2/baleartrek/vendor/swagger-api/swagger-ui/src/core/plugins/json-schema-2020-12-samples/fn/generators/int32.js
+++
@@ -1,6 +0,0 @@
-/**
- * @prettier
- */
-const int32Generator = () => 0
-
-export default int32Generator
--- BLOC2/baleartrek/vendor/swagger-api/swagger-ui/src/core/plugins/json-schema-2020-12-samples/fn/generators/int64.js
+++
@@ -1,6 +0,0 @@
-/**
- * @prettier
- */
-const int64Generator = () => 0
-
-export default int64Generator
